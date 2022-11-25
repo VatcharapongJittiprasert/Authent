f3ca9f0e5b451af219f75230a8537b07660eb489
Inchree <Vatcharapong.j@ku.th>
2022-11-25 14:24:24 +0700

    Change Security to hash

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,8 +21,7 @@ const UserSchema = new mongoose.Schema({
     password: String
 });
 
-UserSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-//! auto encrypt when Find&Save
+// UserSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] }); // //! auto encrypt when Find&Save
 const User = new mongoose.model("User", UserSchema);
 
 //? //////////////////////////////////////////////////
@@ -41,7 +41,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save(function (err) {
         if (!err) {
@@ -52,7 +52,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
     User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
             console.log(err);
